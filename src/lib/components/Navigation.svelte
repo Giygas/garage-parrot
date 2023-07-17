20bb9d06cc42344390bcb45af6b0613d16ac1310
Giygas <gustavorossich@gmail.com>
2023-07-17 05:51:48 +0200

    base navbar animations working

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,25 +1,75 @@
 <script lang="ts">
+	import logo from '$lib/assets/logo.png';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 	const nav = [
 		{ title: 'Accueil', path: '/' },
 		{ title: 'Services', path: '/services' },
 		{ title: "Vehicles d'ocassion", path: '/vehicles' }
 	];
 
-	import logo from '$lib/assets/logo.png';
+	let isScrolled = false;
+
+	onMount(() => {
+		// Add a scroll event listener to track when the user scrolls
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			// Clean up the event listener when the component is unmounted
+			window.removeEventListener('scroll', handleScroll);
+		};
+	});
+
+	function handleScroll() {
+		// Check if the user has scrolled beyond a certain threshold (100 pixels)
+		isScrolled = window.scrollY > 100;
+	}
 </script>
 
-<nav class="w-full md:pe-16 text-primary text-lg">
-	<ul>
-		{#each nav as item}
-			<li>
-				<a href={item.path}>{item.title} </a>
-			</li>
-		{/each}
-	</ul>
-	<img src={logo} alt="Garage logo" />
-</nav>
+<div class="navbar bg-base-100 sticky top-0 {isScrolled ? 'bottom-shadow' : ''}">
+	<div class="flex-1">
+		<a href="/">
+			<img class="logo {isScrolled ? 'shrink' : ''}" src={logo} alt="Garage logo" />
+		</a>
+	</div>
+
+	<div class="navbar-center hidden md:flex justify pt-4">
+		<ul class="menu menu-horizontal px-1 lg:pe-12 text-primary text-base">
+			{#each nav as item}
+				<li>
+					<a
+						href={item.path}
+						class="{$page.url.pathname === item.path ? 'bg-accent text-base-100' : ''} delete"
+						>{item.title}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+
+	<div class="flex-none md:hidden">
+		<button class="btn btn-square btn-ghost">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block w-5 h-5 stroke-current"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4 6h16M4 12h16M4 18h16"
+				/></svg
+			>
+		</button>
+	</div>
+</div>
 
 <style>
+	.delete:active {
+		background-color: #a51d2d50;
+	}
+
 	ul {
 		height: 8vh;
 		align-items: center;
@@ -27,11 +77,42 @@
 		display: flex;
 		flex-direction: row;
 		gap: 5vw;
-		margin-top: 2vh;
 		font-family: 'Montserrat';
 	}
 
 	a:hover {
 		color: #a51d2d;
+	}
+
+	.navbar {
+		align-items: start;
+		padding: 0;
+		margin: 0;
+	}
+
+	.logo {
+		height: 12rem;
+		transition: all 0.3s ease;
+		margin: 8rem auto 0 6rem;
+		max-width: none;
+	}
+
+	.logo.shrink {
+		height: 6rem;
+		margin: 0 0 0 4rem;
+	}
+
+	.bottom-shadow {
+		box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+		transition: box-shadow 0.3s ease-in;
+	}
+
+	@media (max-width: 768) {
+		.logo {
+			height: 8rem;
+		}
+		.logo.shrink {
+			margin: 0 0 0 1rem;
+		}
 	}
 </style>
