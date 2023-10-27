1699c578a715c75b3f4d539160e68277eb67b03e
Giygas <gustavorossich@gmail.com>
2023-10-28 01:44:39 +0200

    Added fetching for google map

--- src/lib/components/Reviews.svelte
+++ src/lib/components/Reviews.svelte
@@ -7,10 +7,25 @@
 </script>
 
 <div
-	class="container review-container flex flex-row shrink-0 overflow-x-auto snap-x snap-mandatory justify-start gap-8 p-5"
+	class="container review-container flex flex-row shrink-0 overflow-x-auto no-scrollbar snap-x snap-mandatory justify-start gap-8 p-5"
 >
 	<Review {...reviews} />
 	<Review {...reviews} />
 	<Review {...reviews} />
 	<Review {...reviews} />
+	<Review {...reviews} />
+	<Review {...reviews} />
+	<Review {...reviews} />
 </div>
+
+<style>
+	.no-scrollbar::-webkit-scrollbar {
+		display: none;
+	}
+
+	/* Hide scrollbar for IE, Edge and Firefox */
+	.no-scrollbar {
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,26 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import type { ReviewType } from '$lib/types';
 	export let data: PageData;
 
-	let reviews = data.revi satisfies ReviewType;
+	let reviews = data.revi;
 
 	import { Reviews } from '$components';
-	import '../app.postcss';
 </script>
 
 <svelte:head>
 	<title>Accueil - Garage Parrot</title>
 </svelte:head>
+<div class="min-h-screen">
+	<div class="md:hidden h-[120px]" />
 
-<div class="md:hidden h-[120px]" />
-
-<div class="flex flex-wrap justify-end md:mt-72 xl:my-32">
-	<div class="text-xl md:bg-primary/10 rounded-xl p-2 md:p-10 m-8 xl:max-w-[40vw]">
-		Chez Garage V. Parrot, nous sommes fiers de vous offrir une large gamme de services pour prendre
-		soin de votre voiture. Avec une expérience de deux ans dans le domaine, nous sommes spécialisés
-		dans la réparation de la carrosserie et de la mécanique des voitures, ainsi que dans leur
-		entretien régulier pour garantir leur performance et leur sécurité.
+	<div class="flex flex-wrap justify-end md:mt-72 xl:my-32">
+		<div class="text-xl md:bg-primary/10 rounded-xl p-2 md:p-10 m-8 xl:max-w-[40vw]">
+			Chez Garage V. Parrot, nous sommes fiers de vous offrir une large gamme de services pour
+			prendre soin de votre voiture. Avec une expérience de deux ans dans le domaine, nous sommes
+			spécialisés dans la réparation de la carrosserie et de la mécanique des voitures, ainsi que
+			dans leur entretien régulier pour garantir leur performance et leur sécurité.
+		</div>
 	</div>
-</div>
 
-<Reviews {reviews} />
+	<Reviews {reviews} />
+</div>
