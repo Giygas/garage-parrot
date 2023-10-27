573c81d92f8fdc8f9e33e92d660b18c03324e801
Giygas <gustavorossich@gmail.com>
2023-10-28 01:02:10 +0200

    Testing font preloading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,21 @@
 	import type { Weekdays } from '$lib/types';
 	export let data;
 	let days: Weekdays = data.days;
+
+	// Preload fonts
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const link = document.createElement('link');
+		link.rel = 'preload';
+		link.href = 'fonts.css';
+		link.as = 'style';
+		link.onload = () => {
+			link.onload = null;
+			link.rel = 'stylesheet';
+		};
+		document.head.appendChild(link);
+	});
 </script>
 
 <head>
