1cb88a1dfd072fb863f1ee769405d93399c9b70d
Giygas <gustavorossich@gmail.com>
2023-10-29 17:16:37 +0100

    Changed hamburguer menu

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -32,15 +32,11 @@
 	}
 </script>
 
-<div
-	class="navbar isolate z-50 backdrop-blur-xl fixed top-0 p-1 md:py-4 align-center justify-center"
->
-	<div class="container relative">
-		<div class="flex-1 justify-start align-top">
-			<a href="/">
-				<img class="logo {isScrolled ? 'shrinked' : ''}" src={logo} alt="Garage logo" />
-			</a>
-		</div>
+<div class="navbar isolate z-50 backdrop-blur-xl fixed top-0 p-1 md:py-4 justify-center px-5">
+	<div class="container relative flex justify-between">
+		<a href="/">
+			<img class="logo {isScrolled ? 'shrinked' : ''}" src={logo} alt="Garage logo" />
+		</a>
 
 		<div class="navbar-center hidden md:flex justify">
 			<ul class="menu menu-horizontal px-1 lg:pe-12 text-primary text-sm lg:text-base">
@@ -56,21 +52,33 @@
 			</ul>
 		</div>
 
-		<div class="flex-none md:hidden max-h-fit">
-			<button class="btn btn-square btn-ghost">
+		<div class="md:hidden">
+			<label class="btn btn-circle swap swap-rotate">
+				<!-- this hidden checkbox controls the state -->
+				<input type="checkbox" />
+
+				<!-- hamburger icon -->
 				<svg
+					class="swap-off fill-current"
 					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					class="inline-block w-10 h-10 stroke-current"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h16M4 18h16"
+					width="32"
+					height="32"
+					viewBox="0 0 512 512"
+					><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg
+				>
+
+				<!-- close icon -->
+				<svg
+					class="swap-on fill-current"
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 512 512"
+					><polygon
+						points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
 					/></svg
 				>
-			</button>
+			</label>
 		</div>
 	</div>
 </div>
