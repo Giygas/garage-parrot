a7e048a0a90f9e2d432a42f41ef98d8f4746aa86
Giygas <gustavorossich@gmail.com>
2024-02-14 13:54:42 +0000

    mobile css done

--- src/routes/(app)/services/+page.svelte
+++ src/routes/(app)/services/+page.svelte
@@ -16,24 +16,23 @@
 	<title>Services - Garage Parrot</title>
 </svelte:head>
 <!-- #TODO: Responsive design here -->
-<section class="container px-8 mb-56">
-	<div class="mt-64 mb-44 px-12">
-		<h1 class="montserrat text-accent text-small-caps text-6xl text-right font-semibold">
-			Nos Services
-		</h1>
+<!-- <div class="mt-52 md:mt-64 xl:mb-44 xl:px-12"> -->
+<div class="md:hidden h-[120px]" />
+<div class="flex flex-wrap pt-5 justify-center text-3xl">
+	<h1 class="montserrat text-accent text-small-caps font-semibold">Nos Services</h1>
+</div>
+{#each services as service}
+	<div class="flex flex-col w-full px-7 mt-10">
+		<!-- <h2 class="montserrat uppercase text-5xl text-primary font-bold tracking-widest mb-14"> -->
+		<h2 class="montserrat uppercase text-primary font-bold self-start text-xl">
+			{service.title}
+		</h2>
+		<br />
+		<p
+			class="self-end text-primary font-normal lg:leading-10 lg:tracking-wide mt-2 ml-6"
+			style="white-space: pre-line;"
+		>
+			{service.description}
+		</p>
 	</div>
-	{#each services as service}
-		<div class="mt-16 h-80">
-			<h2 class="montserrat uppercase text-5xl text-primary font-bold tracking-widest mb-14">
-				{service.title}
-			</h2>
-			<br />
-			<p
-				class="text-3xl text-primary leading-10 font-normal tracking-wide pl-28"
-				style="white-space: pre-line;"
-			>
-				{service.description}
-			</p>
-		</div>
-	{/each}
-</section>
+{/each}
