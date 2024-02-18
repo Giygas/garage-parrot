4081887bf83f93cb0a55f26c175afd538e781278
Giygas <gustavorossich@gmail.com>
2024-02-18 20:48:23 +0000

    bug: bad other images carousel

--- src/routes/(app)/vehicles/detail/[id]/+page.server.ts
+++ src/routes/(app)/vehicles/detail/[id]/+page.server.ts
@@ -1,4 +1,5 @@
 import { db } from '$lib/db/client.js';
+import type { DatabaseVoiture } from '$lib/types.js';
 
 export const load = async ({ params }) => {
 	const title = params.id.split('-').join(' ');
@@ -7,7 +8,25 @@ export const load = async ({ params }) => {
 
 	if (error) throw error;
 
+	const vehicle: DatabaseVoiture = data;
+
+	// Replace the path in the vehicle image for the publicURL
+	if (vehicle) {
+		vehicle.image = await db.storage.from('vehicles').getPublicUrl(vehicle.image).data.publicUrl;
+
+		const otherImages: string[] = [];
+		if (vehicle.other_images) {
+			for (const img of vehicle.other_images) {
+				const i = await db.storage.from('vehicles').getPublicUrl(img).data.publicUrl;
+
+				otherImages.push(i);
+			}
+
+			vehicle.other_images = otherImages;
+		}
+	}
+
 	return {
-		data
+		vehicle: data
 	};
 };
--- src/routes/(app)/vehicles/detail/[id]/+page.svelte
+++ src/routes/(app)/vehicles/detail/[id]/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import { afterNavigate } from '$app/navigation';
+	import { vehicleSchema } from '$lib/schemas';
 	import Icon from '@iconify/svelte';
 	import { onDestroy, onMount } from 'svelte';
 
 	let windowWidth: number;
 	let resizeListener: EventListener;
-
-	let imageBreakpoint: number = 768;
 
 	onMount(() => {
 		if (typeof window !== 'undefined') {
@@ -41,11 +40,9 @@
 		}
 	});
 
-	$: console.log(windowWidth);
-
 	export let data;
 
-	console.log(data);
+	const { vehicle } = data;
 </script>
 
 <svelte:head>
@@ -56,7 +53,7 @@
 	class="flex flex-wrap pt-5 justify-center text-3xl md:text-4xl lg:text-5xl xl:text-6xl md:pt-52 md:justify-end md:pr-12 md:pb-12 lg:pr-24 lg:pt-60 xl:pr-40 xl:pb-24"
 />
 
-<div class="flex flex-row w-full mt-24 md:mt-16 px-5">
+<div class="flex flex-row w-full mt-24 md:mt-16 px-5 mb-10">
 	<button class="flex place-items-center" on:click={() => history.back()}
 		><Icon icon="ic:twotone-keyboard-arrow-left" width="48" height="48" style="color: #6A655C" />
 		<span class="font-montserrat text-xl uppercase font-bold"> Retour </span>
@@ -64,8 +61,26 @@
 </div>
 
 <div class="flex flex-col justify-center">
-	<div class="grid grid-cols-12">
-		<div class="col-span-12 md:col-span-8 bg-blue-100">Image Principale</div>
-		<div class="col-span-12 md:col-span-4 bg-slate-400">Other Images</div>
+	<div class="grid grid-cols-12 h-[576px]">
+		<div class="col-span-12 md:col-span-8 h-[570px]">
+			<img src={vehicle.image} alt="Principal" />
+		</div>
+		<div class="col-span-12 md:col-span-4 bg-slate-400">
+			{#if vehicle.other_images}
+				<div class="carousel carousel-vertical h-full max-h-fit">
+					{#each vehicle.other_images as img}
+						<div class="carousel-item h-1/2">
+							<img src={img} class="w-full" alt="Vehicle" />
+						</div>
+					{/each}
+				</div>
+			{/if}
+		</div>
 	</div>
 </div>
+
+<!-- <style>
+	.carousel {
+		height: 100cqb;
+	}
+</style> -->
