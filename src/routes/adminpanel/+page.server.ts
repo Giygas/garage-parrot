4c43d342eb669ae23353861bce19949bc27694ce
Giygas <gustavorossich@gmail.com>
2025-10-11 21:25:49 +0200

    fix: bad types in function

--- src/routes/adminpanel/+page.server.ts
+++ src/routes/adminpanel/+page.server.ts
@@ -1,12 +1,13 @@
 import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export const load = async (event) => {
-	const user = await event.locals.getUser();
+export const load = (async ({ locals: { getUser, supabase } }) => {
+	const user = await getUser();
 
 	if (!user) {
 		throw redirect(300, '/login');
 	}
 
-	const session = await event.locals.supabase.auth.getSession().then((res) => res.data.session);
+	const session = await supabase.auth.getSession().then((res: any) => res.data.session);
 	return { session };
-};
+}) satisfies PageServerLoad;
